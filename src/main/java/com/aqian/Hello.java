479e2e4abe9e9ecbef01560dc0a744252428aeb8
aqian <2246012574@qq.com>
2023-09-26 16:25:44 +0800

    master 第er次提交

--- src/main/java/com/aqian/Hello.java
+++ src/main/java/com/aqian/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("hello+master");
         System.out.println("hello+hotfix");
+        System.out.println("hello+master");
     }
 }
